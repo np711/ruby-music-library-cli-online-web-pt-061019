f03de596037853b2fe7504b0ad024759ff05c0ed
Nicole Parrish <nicoleparrish.parrish@gmail.com>
2019-08-19 01:06:54 +0000

    Automatically backed up by Learn

--- config/environment.rb
+++ config/environment.rb
@@ -2,8 +2,19 @@ require 'bundler'
 Bundler.require
 
 module Concerns
+  module Findable 
+    def find_by_name(name)
+      self.all.detact {|song| song.name == name}
+  end
+
+def find_or_create_by_name(name)
+  find_by_name(name) || create(name)
   
-end
+end 
+
+end 
+
+end 
 
 require_all 'lib'
 
